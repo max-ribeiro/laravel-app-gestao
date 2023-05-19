4b2372f9c3bb8e19c0804ad02892b9f7838a8b30
Max_Ribeiro <max.ribeiro@simova.com.br>
2023-05-18 21:53:34 -0300

    added the menu as component

--- resources/views/site/main.blade.php
+++ resources/views/site/main.blade.php
@@ -1,20 +1,8 @@
 @extends('site.layouts.basic')
 @section('title', $title)
+
 @section('content')
-<div class="topo">
-    <div class="logo">
-        <img src="{{ asset('img/logo.png') }}">
-    </div>
-
-    <div class="menu">
-        <ul>
-            <li><a href="{{ route('site.index') }}">Principal</a></li>
-            <li><a href="{{ route('site.about') }}">Sobre Nós</a></li>
-            <li><a href="{{ route('site.contact') }}">Contato</a></li>
-        </ul>
-    </div>
-</div>
-
+@include('site.components.top')
 <div class="conteudo-destaque">
 
     <div class="esquerda">
