427d8203bf0362ee51b02426ed973543df4e5861
max-ribeiro <max_ribeiro@live.com>
2023-05-23 22:59:11 -0300

    saving data from form to database

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContact;
 
 class ContactController extends Controller
 {
-    public function show() {
-        var_dump($_POST);
+    public function show(Request $request) {
+
+        $contact = new SiteContact();
+        $contact->name = $request->input('name');
+        $contact->tel =  $request->input('tel');
+        $contact->email = $request->input('email');
+        $contact->subject = $request->input('subject');
+        $contact->message = $request->input('message');
+        $contact->save();
+
         return view('site.contact', ['title' => 'Contato']);
     }
 }
